f96bff344abd5cd6e3336056e516319c349d6306
Md. Anas Khan <108482138+MdAnaskhan0@users.noreply.github.com>
2025-09-18 12:00:06 +0600

    Update lcustomer.sql

--- lcustomer.sql
+++ lcustomer.sql
@@ -1,19 +1,24 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Database Name: loyal_customer
 
+-- Table structure for table `customersinfo`
 CREATE TABLE `customersinfo` (
-  `customerId` int(11) NOT NULL,
+  `customerId` int(11) NOT NULL AUTO_INCREMENT,
   `customerName` varchar(255) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   `branch` varchar(255) DEFAULT NULL,
-  `entryDate` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `entryDate` date DEFAULT NULL,
+  PRIMARY KEY (`customerId`)
+) ENGINE=InnoDB AUTO_INCREMENT=525 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `customersinfo`
-  ADD PRIMARY KEY (`customerId`);
+-- Table structure for table `users`
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `customersinfo`
-  MODIFY `customerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-COMMIT;
+-- Dumping data for table `users`
+INSERT INTO `users` (`id`, `username`, `password`) VALUES ('1', 'admin', 'admin');
 
